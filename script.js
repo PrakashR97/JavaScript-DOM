8f29c73a9fabf08d49a1d6101cbd6b3bc254f8ec
PrakashR97 <prakashramasamy97@gmail.com>
2022-11-28 09:00:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//window.alert("Hello world");
+//window.open("https://google.com");
+//window.prompt("What is your name?");
+//window.moveTo(500,100);
+//window.print("It will Print HTML Page");
+//console.log(console);
+console.warn(console);
